1403eac5b76b09e2c3f63442c346563ced469330
JT-LOVES-TA <jeantheo.samba-fouala@gmail.com>
2021-09-05 13:16:56 +0200

    Credits

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Staking-App
 Simple application emulating staking.
+
+Found this mini project in this video :
+https://www.youtube.com/watch?v=XLahq4qyors&list=WL&index=47&t=8902s
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,11 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
+        <p
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           &nbsp; Staking sim <span role='img' aria-label='Money bag emoji'>💰</span>
-        </a>
+        </p>
 
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
